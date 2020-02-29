88960a215d4921acea841f18f839eda5d3c362fa
Arpit Sharma <arpitsharma@pop-os.localdomain>
2020-03-01 00:18:37 +0530

    Phase 1

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
+const authRoute = require('./routes/auth');
+
+app.use(express.json());
+
+app.use('/api/user', authRoute);
+
+
+dotenv.config();
+
+mongoose.connect(process.env.DB,
+    {useNewUrlParser: true, useUnifiedTopology: true}, 
+     () => console.log('connected to db')
+     );
+
+
+
+app.listen(3000, () => console.log('running'));
